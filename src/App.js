3b1151a12f1d4f6773a3223505bc337c0c78232c
Stefan Petrucev <stefan.petrucev@gmail.com>
2020-11-11 16:34:22 +1100

    Initial commit
    Added create-react-app based timer for Bronjahm spawn.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { PureComponent } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends PureComponent {
+  render() {
+    const currentDate = new Date();
+    const firstDate = new Date(2020, 10, 11, 14, 20, 0); //Month is zero indexed
+
+    let nextDate = firstDate;
+    while (nextDate.getTime() < currentDate.getTime()) {
+      nextDate.setTime(nextDate.getTime() + (6 * 60 + 40) * 60 * 1000);
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Next Bronjahm at
+            <br />
+            {nextDate.toLocaleString()}
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
